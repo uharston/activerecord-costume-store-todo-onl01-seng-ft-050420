b62b5bd79a69bcfeb56d22961b16e07044f55e6a
Uriah Harston <uharston@gmail.com>
2020-06-08 18:55:08 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,13 +1,13 @@
-class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-  def change
-    create_table :haunted_houses do |c|
-      c.string :name
-      c.string :location
-      c.string :theme
-      c.integer :price
-      c.datetime :opening_date
-      c.datetime :closing_date
-    end
-  end
-end
+# class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+#   def change
+#     create_table :haunted_houses do |c|
+#       c.string :name
+#       c.string :location
+#       c.string :theme
+#       c.integer :price
+#       c.datetime :opening_date
+#       c.datetime :closing_date
+#     end
+#   end
+# end
 # Create your haunted_houses migration here
