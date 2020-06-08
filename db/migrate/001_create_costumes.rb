49c168837d20cee77231704e931552de69db29df
Uriah Harston <uharston@gmail.com>
2020-06-08 18:55:13 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,14 +1,14 @@
-class CreateCostumes < ActiveRecord::Migration[4.2]
-  def change
-    create_table :costumes do |c|
-      c.string :name
-      c.float :price
-      c.string :size
-      c.string :image_url
-      c.timestamps
-    end
-  end
-end
+# class CreateCostumes < ActiveRecord::Migration[4.2]
+#   def change
+#     create_table :costumes do |c|
+#       c.string :name
+#       c.float :price
+#       c.string :size
+#       c.string :image_url
+#       c.timestamps
+#     end
+#   end
+# end
 
 # Create a class and inherit from ActiveRecord::Migration
 
